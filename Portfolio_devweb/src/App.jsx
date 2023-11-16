3c2d305d66a4b9197bda80f165cfba3777242942
unknown <alexandradepourtoux93@gmail.com>
2023-11-16 20:36:21 +0100

    création portfolio

---
+++ Portfolio_devweb/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style/index.scss";
+import MainPage from "./components/MainPage"
+
+
+function App() {
+
+
+  return (
+    <>
+    <MainPage />
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio_devweb/src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+function MainPage () {
+
+    return (
+        <>
+        <div className="Header_text">
+        <h1>Welcome</h1>
+        <p>to</p>
+        <h2>my Portfolio</h2>
+        </div>
+        
+        </>
+    )
+}
+
+export default MainPage
