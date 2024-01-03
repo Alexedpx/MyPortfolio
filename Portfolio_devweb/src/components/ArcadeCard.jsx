c234eba040a4e59c2a1d07d102a122b7006f95b3
Alexandra <alexandradepourtoux93@gmai.com>
2024-01-03 12:22:24 +0100

    ajout carte arcade

---
+++ Portfolio_devweb/src/components/ArcadeCard.jsx
@@ -0,0 +1,18 @@
+export default function ArcadeCard() {
+  return (
+    <>
+      <div className="Arcade">
+        <div className="innerCard">
+          <div className="frontSide">
+            {/* <img src="/images/card/Noël.png" /> */}
+          </div>
+          <div className="backSide">
+            <p className="title">Arcade Space</p>
+            <h1>Work in progress ...</h1>
+            <div className="description"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- Portfolio_devweb/src/pages/ProjectsList.jsx
+++ Portfolio_devweb/src/pages/ProjectsList.jsx
@@ -1,6 +1,7 @@
 import SharingCard from "../components/SharingCard";
 import WookieCard from "../components/WookieCard";
 import HackathonCard from "../components/HackathonCard";
+import ArcadeCard from "../components/ArcadeCard";
 import Logo from "../components/Logo";
 import Footer from "../components/Footer";
 import { useState } from "react";
@@ -14,41 +15,48 @@ export default function ProjectsList() {
 
   return (
     <>
-     <div class="bg"></div>
+      <div class="bg"></div>
       <div class="bg bg2"></div>
       <div class="bg bg3"></div>
       <div class="content"></div>
-    <div className="container-presentation">
-         <Logo />
-         </div>
+      <div className="container-presentation">
+        <Logo />
+      </div>
       <div className="container-card">
         {openCard === "SharingCard" && <SharingCard />}
         {openCard === "WookieCard" && <WookieCard />}
         {openCard === "HackathonCard" && <HackathonCard />}
+        {openCard === "ArcadeCard" && <ArcadeCard />}
         <div className="project-list">
-        <div
-          className="sharing-project"
-          onClick={() => handleClickShowCard("SharingCard")}
-        >
-          <p>Sharing is Caring</p>
-          <br></br>
-        </div>
-        <div
-          className="wookie-project"
-          onClick={() => handleClickShowCard("WookieCard")}
-        >
-          <p>Wookie Taxi</p>
-          <br></br>
+          <div
+            className="sharing-project"
+            onClick={() => handleClickShowCard("SharingCard")}
+          >
+            <p>Sharing is Caring</p>
+            <br></br>
           </div>
           <div
-          className="hackathon-project"
-          onClick={() => handleClickShowCard("HackathonCard")}
-        >
-          <p>Noël en un clic</p>
+            className="wookie-project"
+            onClick={() => handleClickShowCard("WookieCard")}
+          >
+            <p>Wookie Taxi</p>
+            <br></br>
           </div>
-         
-      </div>
-      
+          <div
+            className="hackathon-project"
+            onClick={() => handleClickShowCard("HackathonCard")}
+          >
+            <p>Noël en un clic</p>
+            <br></br>
+          </div>
+
+          <div
+            className="Arcade-project"
+            onClick={() => handleClickShowCard("ArcadeCard")}
+          >
+            <p>Arcade Realm</p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
